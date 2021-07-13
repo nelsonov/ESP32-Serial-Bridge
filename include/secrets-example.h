e2bbaa95094e7027dcec65e5322afbc8a55c5e8d
Lee Nelson <lnelson@nelnet.org>
2021-07-12 22:40:24 -0700

    Add secrets-example.h

---
+++ include/secrets-example.h
@@ -0,0 +1,5 @@
+// Copy this file to secrets.h and modify secrets.h as needed
+// secrets.h is ignored by git for security reasons
+
+const char *ssid = "myssid";  // You will connect your phone to this Access Point
+const char *pw = "mypass"; // and this is the password
